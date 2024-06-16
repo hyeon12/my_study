7a961612b9fc6b7d39bbf54f250d7bbfb8bd522e
hyeon12 <seojh912_@naver.com>
2024-06-16 16:05:46 +0900

    Chapter11 기본클래스

---
+++ chapter11/src/clazz/ClassEx.java
@@ -0,0 +1,9 @@
+package clazz;
+
+public class ClassEx {
+    public static void main(String[] args) {
+        String s = new String();
+        Class c = s.getClass();
+        System.out.println(c); //class java.lang.String
+    }
+}
